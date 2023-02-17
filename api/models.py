fb42fa5418e272e9dd063941abbd8d463106ecaa
Swethapy <125079992+Swethapy@users.noreply.github.com>
2023-02-16 22:11:36 -0500

    Update models.py

--- api/models.py
+++ api/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Employee(models.Model):
     emp_id = models.IntegerField(primary_key=True)
     emp_name = models.CharField(max_length=100)
-    manager = models.ForeignKey('self', on_delete=models.SET_NULL, null=True, blank=True, related_name='employees')
+    manager_name = models.ForeignKey('self', on_delete=models.SET_NULL, null=True, blank=True, related_name='employees')
 
     def __str__(self):
         return f"{self.emp_name}"
