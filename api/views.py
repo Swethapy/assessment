7dead5d3694adfebc10bc01b377c0fa582c8ffb0
Swethapy <125079992+Swethapy@users.noreply.github.com>
2023-02-16 22:10:53 -0500

    Update views.py

--- api/views.py
+++ api/views.py
@@ -1,14 +1,15 @@
-from rest_framework.generics import ListAPIView
+from rest_framework import viewsets
+
 from .models import Employee
-from .serializers import EmployeeSerializer
+from .serializers import ReadEmployeeSerializer, WriteEmployeeSerializer
 
 
-class EmployeeList(ListAPIView):
-    """
-    API Endpoint: http://127.0.0.1:8000/api/employees/
-    This is a class-based view inheriting from generic ListAPIView.
-    This GET API Endpoint will return all the Employee objects/records from the DB
-    and order them based on emp_id in an ascending order
-    """
-    queryset = Employee.objects.order_by('emp_id').all()
-    serializer_class = EmployeeSerializer
+class EmployeesViewSet(viewsets.ModelViewSet):
+    queryset = Employee.objects.all()
+
+    def get_serializer_class(self):
+        if self.action in ("list", "retrieve"):
+            return ReadEmployeeSerializer
+
+        return WriteEmployeeSerializer
+
