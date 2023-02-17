fd85b88afb9752199cfe32610706502f79c52224
Swethapy <125079992+Swethapy@users.noreply.github.com>
2023-02-16 22:10:30 -0500

    Update serializers.py

--- api/serializers.py
+++ api/serializers.py
@@ -2,9 +2,27 @@ from rest_framework import serializers
 from .models import Employee
 
 
-class EmployeeSerializer(serializers.ModelSerializer):
-    manager_name = serializers.StringRelatedField(source='manager.emp_name', read_only=True)
+class ReadEmployeeSerializer(serializers.ModelSerializer):
+    manager_name = serializers.StringRelatedField()
+
+    class Meta:
+        model = Employee
+        fields = ('emp_id', 'emp_name', 'manager_name')
+        read_only_fields = fields
+
+
+class WriteEmployeeSerializer(serializers.ModelSerializer):
+    manager_name = serializers.SlugRelatedField(slug_field="emp_name", allow_null=True, queryset=Employee.objects.all())
 
     class Meta:
         model = Employee
         fields = ['emp_id', 'emp_name', 'manager_name']
+
+
+class EmployeeSerializer(serializers.ModelSerializer):
+    manager_name = serializers.StringRelatedField()
+
+    class Meta:
+        model = Employee
+        fields = ['emp_id', 'emp_name', 'manager_name']
+
