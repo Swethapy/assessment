97ff485200db7b094d51fd251d26ce6d8f3a9781
Swethapy <125079992+Swethapy@users.noreply.github.com>
2023-02-16 22:11:13 -0500

    Update urls.py

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,13 @@
-from django.urls import path
-from .views import EmployeeList
+from django.urls import path, include
+from .views import EmployeesViewSet
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register(r'employees', EmployeesViewSet)
 
 app_name = 'api'
 
 urlpatterns = [
     # api/employees/
-    path('employees/', EmployeeList.as_view(), name='employees'),
+    path('', include(router.urls)),
 ]
